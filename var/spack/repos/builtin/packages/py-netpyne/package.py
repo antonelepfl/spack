a40bc50524d7f76f28e5343d159de41c45dbaea0
Ioannis <ioannis.magkanaris@epfl.ch>
2020-07-13 19:19:16 +0300

    Fixed documentation

--- var/spack/repos/builtin/packages/py-netpyne/package.py
+++ var/spack/repos/builtin/packages/py-netpyne/package.py
@@ -1,30 +1,14 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+##############################################################################
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install example
-#
-# You can edit this file again by typing:
-#
-#     spack edit example
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
 
 class PyNetpyne(PythonPackage):
-    """Netpyne: A python package to facilitate the development, parallel simulation, 
+    """Netpyne: A python package to facilitate the development, parallel simulation,
     optimization and analysis of multiscale biological neuronal networks in NEURON."""
 
     homepage = "http://www.netpyne.org/"
